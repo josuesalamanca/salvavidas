e67c905f629d7144c0ccaf0b368293908a0540cf
Josue <josuesalamanca3@gmail.com>
2018-04-17 21:27:34 -0500

    nuevos cambios

--- calculadora/calculadora.cpp
+++ calculadora/calculadora.cpp
@@ -13,38 +13,57 @@ int main(int argc, char** argv) {
 	printf("para radicar digite 7\n");
 	printf("para log en base 10 digite 8\n");
 	printf("para logaritmo natural digite 9\n");
-	printf("--------------------------------------------------------------------------------");
+	printf("--------------------------------------------------------------------------------\n");
 	
 	printf("digite el numero seguido del numero de la operacion y luego digite el segundo numero\n");
 	
-	printf("--------------------------------------------------------------------------------");	
+	printf("--------------------------------------------------------------------------------\n");	
 //	--------------------------------------------------------------
 	
 	int g,h;
-	float a,b,c,s;
+	float a,b,c,s,x,ans; 
+	
+	ans=0;
 
+	for ( x=1;x<32000;x++){
+	
 	scanf("%f",&a);
+	
+		if (a==777){
+		a=ans;
+	}
+	
 	scanf("%f",&s);
 	scanf("%f",&b);
-	c=0;
+	
+
+	
+	
+	
+	
+	
 	if(s=1){
 		c=a+b;
 		printf("la suma es=%f\n",c);
+		ans=c;
 	}
 	
 	else if(s=2){
 		c=a-b;
 		printf("la resta es=%f\n",c);
+		ans=c;
 	}
 	
 	else if(s=3){
 		c=a*b;
 		printf("la multiplicacion es=%f\n",c);
+		ans=c;
 	}
 	
 	else if(s=4){
 		c=a/b;
 		printf("la divicion es=%f\n",c);
+		ans=c;
 	}
 	
 	else if(s=5){
@@ -52,26 +71,32 @@ int main(int argc, char** argv) {
 		b=h;
 		c=g%h;
 		printf("el modulo es=%f\n",c);
+		ans=c;
 	}
 	
 	else if(s=6){
 		c=pow(a,b);
 		printf("la potencia es=%f\n",c);
+		ans=c;
 	}
 	
 	else if(s=7){
 		c=pow(a,1/b);
 		printf("la raiz es=%f\n",c);
+		ans=c;
 	}
 	
 	else if(s=8){
 		c=a*log10(b);
 		printf("la suma es=%f\n",c);
+		ans=c;
 	}
 	
 	else if(s=9){
 		c=a*log(b);
 		printf("la suma es=%f\n",c);
+		ans=c;
 	}
+}
 	return 0;
 }
