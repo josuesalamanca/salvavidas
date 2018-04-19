df51e6f61fc4ccdb337debc56dad77e60d069db6
Marcelo Perez <lordfomalhaut@gmail.com>
2018-04-19 12:14:57 -0500

    funcional

--- calculadora/calculadora.cpp
+++ calculadora/calculadora.cpp
@@ -1,102 +1,130 @@
-#include <iostream>
+#include <stdio.h>
 #include <math.h>
 
 
 int main(int argc, char** argv) {
-	
-	printf("para sumar digite 1\n");
-	printf("para restar digite 2\n");
-	printf("para multiplicar digite 3\n");
-    printf("para dividir digite 4\n");
-	printf("para modular digite 5\n");
-	printf("para potenciar digite 6\n");
-	printf("para radicar digite 7\n");
-	printf("para log en base 10 digite 8\n");
-	printf("para logaritmo natural digite 9\n");
+	printf("--------------------------------------------------------------------------------\n");
+
+	printf("digite el numero seguido de la letra de la operacion y luego digite el segundo numero\n");
+
+	printf("--------------------------------------------------------------------------------\n");
+
+	printf("para sumar digite m\n");
+	printf("para restar digite n\n");
+	printf("para multiplicar digite x\n");
+    printf("para dividir digite d\n");
+	printf("para sacar el porcentaje digite o\n");
+	printf("para potenciar digite p\n");
 	printf("--------------------------------------------------------------------------------\n");
 	
-	printf("digite el numero seguido del numero de la operacion y luego digite el segundo numero\n");
+	printf("para radicar digite r\n");
+	printf("para log en base 10 digite l\n");
+	printf("recuerde que el primer digito multiplicara la operacion\n");
 	
-	printf("--------------------------------------------------------------------------------\n");	
+	printf("--------------------------------------------------------------------------------\n");
+	printf("para sacar el seno digite s\n");
+	printf("para sacar el coseno digite c\n");
+	printf("para sacar la tangente digite t\n");
+	
+	printf("recuerde que el primer digito multiplicara la operacion\n");
+	printf("--------------------------------------------------------------------------------\n");
+	printf("para usar su ultima respuesta su primer numero debe ser 0\n");
+	
+	printf("--------------------------------------------------------------------------------\n");
+
+	printf("digite el numero seguido de la letra de la operacion y luego digite el segundo numero\n");
+
+	printf("--------------------------------------------------------------------------------\n");
 //	--------------------------------------------------------------
-	
-	int g,h;
-	float a,b,c,s,x,ans; 
-	
+
+	int g,h,y;
+	float a,b,c,x,ans;
+	char s;
+	y=0;
 	ans=0;
 
 	for ( x=1;x<32000;x++){
-	
-	scanf("%f",&a);
-	
-		if (a==777){
-		a=ans;
-	}
-	
-	scanf("%f",&s);
-	scanf("%f",&b);
-	
+		
+		
+	scanf("%f", &a);
+			if(a==0){
+				a=ans;
+			}
 
+	scanf("%c", &s);
+	scanf("%f", &b);
 	
-	
-	
-	
-	
-	if(s=1){
-		c=a+b;
-		printf("la suma es=%f\n",c);
-		ans=c;
-	}
-	
-	else if(s=2){
-		c=a-b;
-		printf("la resta es=%f\n",c);
-		ans=c;
-	}
-	
-	else if(s=3){
-		c=a*b;
-		printf("la multiplicacion es=%f\n",c);
-		ans=c;
-	}
-	
-	else if(s=4){
-		c=a/b;
-		printf("la divicion es=%f\n",c);
-		ans=c;
-	}
-	
-	else if(s=5){
-		a=g;
-		b=h;
-		c=g%h;
-		printf("el modulo es=%f\n",c);
-		ans=c;
-	}
-	
-	else if(s=6){
-		c=pow(a,b);
-		printf("la potencia es=%f\n",c);
-		ans=c;
-	}
-	
-	else if(s=7){
-		c=pow(a,1/b);
-		printf("la raiz es=%f\n",c);
-		ans=c;
-	}
-	
-	else if(s=8){
-		c=a*log10(b);
-		printf("la suma es=%f\n",c);
-		ans=c;
-	}
-	
-	else if(s=9){
-		c=a*log(b);
-		printf("la suma es=%f\n",c);
-		ans=c;
-	}
-}
+			
+			if (s=='m'){
+			
+			c=a+b;
+		
+				printf("la suma es %f\n ",c);
+			ans=c;	
+			}
+			
+			if (s=='n'){
+			c=a-b;
+				printf("la resta es %f\n ",c);
+			ans=c;	
+			}
+			
+			if (s=='x'){
+			c=a*b;
+				printf("la multiplicacion es %f\n ",c );
+			ans=c;	
+			}
+			
+			if (s=='d'){
+				if(a==0){
+					printf("0");
+				}
+				else{
+					c=a/b;
+					printf("la division es %f\n ",c);
+					ans=c;
+				}	
+			}
+			
+			if (s=='o'){
+				
+			c=(b*a)/100;
+				printf("el porcentaje es %f\n ",c);
+			ans=c;	
+			}
+			
+			if (s=='p'){
+			c=pow(a,b);
+				printf("la potenciacion es %f\n ",c);
+			ans=c;	
+			}
+			if (s=='r'){
+			c=a*sqrt(b);
+				printf("la raiz es %f\n ",c);
+			ans=c;	
+			}	
+			if (s=='l'){
+			c=a*log10(b);
+				printf("el logaritmo es %f\n ",c);
+			ans=c;	
+			}
+			if (s=='s'){
+			c=a*sin(b);
+				printf("el seno en radianes es %f\n ",c);
+			ans=c;	
+			}
+			if (s=='c'){
+			c=a*cos(b);
+				printf("el coseno en radianes es %f\n ",c);
+			ans=c;	
+			}
+			if (s=='t'){
+			c=a*tan(b);
+				printf("la tangente en radianes es %f\n ",c);
+			ans=c;	
+			}
+		}
+
 	return 0;
 }
+	
